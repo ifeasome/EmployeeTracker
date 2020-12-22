31a8f4d8a68ac2468f2f53e7c2efab9d4170646a
Somelina Obiechina <ifeasome@gmail.com>
2020-12-21 23:38:33 -0600

    Create seeds.sql

---
+++ Database/seeds.sql
@@ -0,0 +1,20 @@
+USE EmployeesDB;
+
+
+INSERT INTO department (name)
+VALUES ("Legal"),
+       ("Accounting & Finance"),
+       ("Human Resources"),
+       ("Research & Development"),
+       ("Sales"),
+       ("Production"),
+;
+
+INSERT INTO role (title, salary, department_id, )
+VALUES ("TRNDSTTR","Black Coast", "Dance");
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Who's Next", "The Who", "Classic Rock");
+
+
+
