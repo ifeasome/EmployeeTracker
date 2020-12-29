c50103de05363f2ebcfe4db37fc7144c6b447813
Somelina Obiechina <ifeasome@gmail.com>
2020-12-28 22:57:15 -0600

    Update query.js

--- db/query.js
+++ db/query.js
@@ -13,58 +13,45 @@ const knex = require('knex') (options);
 
 const cTable = require('console.table');
 
-let  app = require("../app.js");
 
-app.start();
 
 // exported functions
 let queryCalls = {
   // function Employee View
   empFunc: function() {
-    knex.from('employee').select("*")
-    .then((output) => {
-            console.table(output);
-        }
-    )
-    .catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    })
+    return knex.from('employee').select("*")
   }, 
   // function Department View
   deptFunc: function() {
-    knex('department')
+   return knex('department')
     .join('role', 'department.id', 'role.department_id' )
     .select('department.id', 'department.name', 'role.title', 'role.salary')
-    .then((output) => {
-            console.table(output);
-        }
-    ).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    })
   },
   // function Roles View
   rolesView: function() {
-    knex('role')
+    return knex('role')
     .join('employee', 'role.id', 'employee.role_id')
     .select('role.id', 'role.title', 'employee.first_name', 'employee.last_name', 'role.salary')
-    .then((output) => {
-            console.table(output);
-        }
-    ).catch((err) => { console.log( err); throw err })
-    .finally(() => {
-        knex.destroy();
-    })
+  }, 
+  // function Department View by salary totals
+  deptSalaryView: function() {
+    return knex('department')
+    .join('role', 'department.id', 'role.department_id')
+    .select('department.id', 'role.salary', 'department.name')
+  },
+  // function Employee View by Managers
+  empmanagerView: function() {
+    return knex()
   }
+
 };
 module.exports = { queryCalls };
 
 
 
-// deptSalaryView();
 
-// empmanagerView();
+
+
 
 // addDept();
 
